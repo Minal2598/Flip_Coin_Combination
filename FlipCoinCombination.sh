0cb425e7e6d288c2d22679515959303ba1a26034
Minal <patilminalp@gmail.com>
2021-06-05 17:02:35 +0530

    singlet Head and Tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,24 @@
-echo "Welcome in Flip Coin Combination"
+echo "Welcome in Flip Coin Combination Problem"$'\n'
 
-Result=$((RANDOM%2))
-if [ ${Result} -eq 0 ]
-then
-    echo Heads
-elif [ ${Result} -eq 1 ]
-then
-    echo Tails
-fi
+declare -A singlet
+
+singleH=0
+singleT=0
+
+for (( i = 0; i < 100; i++ )); do
+	
+	flip=$(( RANDOM%2 ))
+
+	if [[ $flip -eq 0 ]]; then
+		singlet[$i]=0
+		((singleH++))
+	else
+		singlet[$i]=1
+		((singleT++))
+	fi
+
+done
+
+echo "Percentage of Heads for Singlet Dictionary:" $singleH%
+echo "Percentage of Tails for Singlet Dictionary:" $singleT% $'\n'
+
